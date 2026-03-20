01110eeda383d4ffec61cf2ace51d7441be567cc
Micha Sengotta <micha.sengotta@tngtech.com>
2026-03-20 09:23:57 +0100

    Add the notes plugin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Notes from "reveal.js/plugin/notes/notes.esm.js";
 
 let deck = new Reveal({
-  plugins: [Markdown],
+  plugins: [Markdown, Notes],
   history: true,
 });
 deck.initialize();
