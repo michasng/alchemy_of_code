1c3206c2165b6c6a923e1836a81db6f94b7460be
Micha Sengotta <micha.sengotta@tngtech.com>
2026-03-20 10:42:19 +0100

    Enable syntax highlighting

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import Reveal from "reveal.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import Notes from "reveal.js/plugin/notes/notes.esm.js";
 
+import "reveal.js/plugin/highlight/monokai.css";
+
 let deck = new Reveal({
-  plugins: [Markdown, Notes],
+  plugins: [Markdown, Notes, Highlight],
   history: true,
   autoAnimate: true,
 });
